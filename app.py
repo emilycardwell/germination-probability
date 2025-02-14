0af58d4e5aef0ba2ecb6dd02f95801500262ed0f
Emily Cardwell <emily@emilycardwell.com>
2025-02-14 16:26:27 +0100

    fixed latency issues

--- app.py
+++ app.py
@@ -5,154 +5,172 @@ import math
 
 @st.cache_data
 def get_seed_count(p, k, confidence):
-    n = int(k / p)
 
-    inv_prob = 0
-    while inv_prob < confidence:
-        cum_prob = 0
-        for i in range(k):
-            log_a = math.log(math.comb(n, i))
-            log_b = i * math.log(p)
-            log_c = (n-i) * math.log(1-p)
-            log_prob = log_a + log_b + log_c
-            if log_prob < -100:
-                prob = 0
-            else:
-                prob = math.exp(log_prob)
-            cum_prob += prob
+    def calc_seeds(p, k, confidence):
+        n = int(k / p)
+        inv_prob = 0
+        while inv_prob < confidence:
+            cum_prob = 0
+            for i in range(k):
+                log_a = math.log(math.comb(n, i))
+                log_b = i * math.log(p)
+                log_c = (n-i) * math.log(1-p)
+                log_prob = log_a + log_b + log_c
+                if log_prob < -100:
+                    prob = 0
+                else:
+                    prob = math.exp(log_prob)
+                cum_prob += prob
+            inv_prob = 1 - cum_prob
+            n += 1
+        seeds = n - 1
+        return inv_prob, seeds
 
-        inv_prob = 1 - cum_prob
-        n += 1
+    if k > 250:
+        mult = k//250
+        rem = k % 250
+        k_prob, k_seeds = calc_seeds(p, 250, confidence)
+        r_prob, r_seeds = calc_seeds(p, rem, confidence)
+        f_seeds = (k_seeds * mult) + r_seeds
+        f_prob = min(k_prob, r_prob)
+        return f_prob, f_seeds
+    else:
+        return calc_seeds(p, k, confidence)
 
-    seeds = n - 1
+def body():
+    # Background
+    st.html(
+        """
+        <style>
+            .stApp {
+                background-image: url(https://i.postimg.cc/jdsrt0B8/app-background-opaque.png);
+                background-size: cover;
+            }
 
-    return seeds, inv_prob
+            h1 {
+                text-align: center;
+                font-size: 42px;
+                font-weight:600;
+            }
 
+            h2 {
+                text-align: center;
+                font-size: 18px;
+                font-style: italic;
+                font-weight:300;
+                line-height: 1.0;
+                color: darkgreen;
+            }
 
-# Background
-st.markdown(
-    """
-    <style>
-        .stApp {
-            background-image: url(https://i.postimg.cc/jdsrt0B8/app-background-opaque.png);
-            background-size: cover;
-        }
+            h3 {
+                font-size: 18px;
+                line-height: 1.2;
+                font-weight:400;
+            }
 
-        h1 {
-            text-align: center;
-            font-size: 42px;
-            font-weight:600;
-        }
-
-        h2 {
-            text-align: center;
-            font-size: 14px;
-            font-style: italic;
-            font-weight:300;
-            line-height: 1.0;
-            color: darkgreen;
-        }
-
-        h3 {
-            font-size: 18px;
-            line-height: 1.2;
-            font-weight:400;
-        }
-
-        body {
-            font-size: 12px;
-            line-height: 1.0;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
+            body {
+                font-size: 14px;
+                line-height: 1.0;
+                color: darkgreen;
+            }
+        </style>
+        """
     )
 
-# Title & Subtitle
-heading = '''
-    <h1>Germination Calculator</h1>
-    <h2>Calculate the number of seeds you should plant</h2>
-    '''
-st.markdown(heading, unsafe_allow_html=True)
-st.markdown('#####')
-
-# intput seedlings
-row1 = st.columns([0.7, 0.1, 0.2])
-
-with row1[0]:
-    prompt1 = '''
-        <h3>Enter the number of seedlings you want</h3>
-        <body>Any numbers larger than 250 will take exponentially longer to compute</body>
+    # Title & Subtitle
+    st.html(
         '''
-    st.markdown(prompt1, unsafe_allow_html=True)
-    with row1[2]:
-        st.markdown('')
-        desired_plants = st.number_input('Enter the number of seedlings you want', min_value=1, max_value=1000, value=10, label_visibility='collapsed')
-
-st.markdown('######')
-
-# input germination rate
-row2 = st.columns([0.7, 0.1, 0.2])
-with row2[0]:
-    prompt2 = '''
-        <h3>Enter the germination rate of the seeds</h3>
-        <body>If 60%, enter 0.60</body>
+        <h1>Germination Calculator</h1>
+        <h2>Calculate the number of seeds you should sow for X number of seedlings</h2>
+        <br>
         '''
-    st.markdown(prompt2, unsafe_allow_html=True)
-    with row2[2]:
-        st.markdown('')
-        germination_rate = st.number_input('Enter the germination rate of the seeds', min_value=0.0, max_value=1.0, value=0.6, label_visibility='collapsed')
+    )
 
-st.markdown('######')
+    # intput seedlings
+    row1 = st.columns([0.7, 0.1, 0.2])
 
-# input confidence interval
-row3 = st.columns([0.7, 0.1, 0.2])
-with row3[0]:
-    prompt3 = '''
-        <h3>Enter the minimum confidence interval</h3>
-        <body>Recommended: 95%. The closer to 100%, the slower you will get your result.</body>
-        '''
-    st.markdown(prompt3, unsafe_allow_html=True)
-    with row3[2]:
-        st.markdown('')
-        confidence_interval = st.number_input('Enter the confidence interval', min_value=0.0, max_value=1.0, value=0.95, label_visibility='collapsed')
+    with row1[0]:
+        prompt1 = '''
+            <h3>Enter the number of seedlings you want</h3>
+            '''
+        st.html(prompt1)
+        with row1[2]:
+            st.markdown('')
+            desired_plants = st.number_input('Enter the number of seedlings you want',
+                                            min_value=1, max_value=10000, value=10,
+                                            label_visibility='collapsed')
 
-st.markdown('###')
+    st.markdown('######')
 
-if st.button("Get your estimated number of seeds to plant"):
-    try:
-        seeds, final_probability = get_seed_count(germination_rate, desired_plants, confidence_interval)
+    # input germination rate
+    row2 = st.columns([0.7, 0.1, 0.2])
+    with row2[0]:
+        prompt2 = '''
+            <h3>Enter the germination rate of the seeds (60% = 0.6)</h3>
+            '''
+        st.html(prompt2)
+        with row2[2]:
+            st.markdown('')
+            germination_rate = st.number_input('Enter the germination rate of the seeds',
+                                            min_value=0.1, max_value=1.0, value=0.6,
+                                            label_visibility='collapsed')
 
-        st.markdown('')
+    st.markdown('######')
 
-        response_row = st.columns([0.7, 0.1, 0.2])
-        with response_row[0]:
-            response1 = f'''
-                <h3 style="
-                    color: green;
-                    font-size: 17px;
-                    font-weight: 500;
-                    line-height: 2.0;">
-                To have a {round(final_probability*100, 2)}% chance of getting \
-                {desired_plants} seedlings, you should plant:
-                </h3>
-                '''
-            st.markdown(response1, unsafe_allow_html=True)
-            with response_row[2]:
-                response2 = f'''
-                    <p style="
-                        color: darkred;
-                        background-color: white;
-                        border: 1px solid black;
-                        line-height: 2.0;
-                        font-size: 24px;
-                        text-align: center;">
-                    {seeds} seeds
-                    </p>
+    # input confidence interval
+    row3 = st.columns([0.7, 0.1, 0.2])
+    with row3[0]:
+        prompt3 = '''
+            <h3>Enter the minimum confidence interval (95% recommended)</h3>
+            '''
+        st.html(prompt3)
+        with row3[2]:
+            st.markdown('')
+            confidence_interval = st.number_input('Enter the confidence interval',
+                                                min_value=0.50, max_value=0.99, value=0.95,
+                                                label_visibility='collapsed')
+
+    st.markdown('###')
+
+    if st.button("Get your estimated number of seeds to plant"):
+        try:
+            final_probability, seeds = get_seed_count(germination_rate, desired_plants, confidence_interval)
+
+            response_row = st.columns([0.7, 0.3])
+            with response_row[0]:
+                response1 = f'''
+                    <h3 style="
+                        color: green;
+                        font-size: 17px;
+                        font-weight: 500;
+                        line-height: 2.0;">
+                    To have a {round(final_probability*100, 2)}% chance of getting \
+                    {desired_plants} seedlings, you should plant:
+                    </h3>
                     '''
-                st.markdown(response2, unsafe_allow_html=True)
-    except:
-        st.markdown('Sorry, an unknown error occurred. If the error continues, please send an email to emily@emilycardwell.com')
+                st.html(response1)
+                with response_row[1]:
+                    response2 = f'''
+                        <p style="
+                            color: darkred;
+                            background-color: white;
+                            border: 1px solid black;
+                            line-height: 2.0;
+                            font-size: 24px;
+                            text-align: center;">
+                        {seeds} seeds
+                        </p>
+                        '''
+                    st.html(response2)
+
+        except:
+            st.html(
+                '''
+                <body>An unknown error occurred. Check parameters.<body>
+                <body>If the error persists, please make an issue <body>\
+                <a href="https://github.com/emilycardwell/germination-probability/issues">here</a>
+                '''
+            )
 
 def layout(*args):
 
@@ -193,7 +211,7 @@ def layout(*args):
         body
     )
 
-    st.markdown(style, unsafe_allow_html=True)
+    st.html(style)
 
     for arg in args:
         if isinstance(arg, str):
@@ -202,7 +220,7 @@ def layout(*args):
         elif isinstance(arg, HtmlElement):
             body(arg)
 
-    st.markdown(str(foot), unsafe_allow_html=True)
+    st.html(str(foot))
 
 def link(link, text, **style):
     return a(_href=link, _target="_blank", style=styles(**style))(text)
@@ -216,4 +234,5 @@ def footer():
 
 
 if __name__ == "__main__":
+    body()
     footer()
